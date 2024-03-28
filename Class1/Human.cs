804db95197c75b70956f690b4d60def3f2a5e5a3
Ali Aliyev <elvinaliyev724@gmail.com>
2024-03-28 16:27:38 +0400

    first

---
+++ Class1/Human.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class1
+{
+    public class Human
+    {
+        #region Human
+
+        public string Name;
+        public string SurName;
+        public int Age;
+        public string City;
+
+        public Human(string name, string surname, int age, string city)
+        {
+            Name = name;
+            SurName = surname;
+            Age = age;
+            City = city;
+            
+        }
+        public Human()
+        {
+            
+        }
+
+
+
+        public bool IsStudent()
+        {
+            return Age >= 18 && Age <= 25;
+        }
+
+        public void CheckCity(string city1,string city2)
+        {
+            if (city1 == city2) Console.WriteLine("Uygun");
+            else Console.WriteLine("Deyil");
+        }
+
+
+
+        //public void GetCount(Human[] humans)
+        //{
+        //    //foreach (var item in humans)
+        //    //{
+        //    //    Console.WriteLine(item.City);
+        //    //}
+        //    int count = 0;
+        //    for (int i = 0; i < humans.Length; i++)
+        //    {
+        //        Console.WriteLine(humans[i].City);
+
+        //    }
+        //}
+
+        public (string, int,string) GetCount(Human[] insanlar) 
+        {
+            int count = 0;
+            string telebeadlari = "";
+            string muqayiseseher = City.ToLower(); 
+            foreach (var insan in insanlar)
+            {
+                string muqayiseinsan = insan.City.ToLower(); 
+                if (Muqayise(muqayiseinsan, muqayiseseher))
+                {
+                    count++;
+                    if (telebeadlari != "")
+                    {
+                        telebeadlari += ", ";
+                    }
+                    telebeadlari += insan.Name + " " + insan.SurName;
+                }
+            }
+            return (City, count,telebeadlari);
+        }
+
+        
+        private bool Muqayise(string str1, string str2)
+        {
+            if (str1.Length != str2.Length)
+                return false;
+
+            for (int i = 0; i < str1.Length; i++)
+            {
+                if (char.ToLower(str1[i]) != char.ToLower(str2[i]))
+                    return false;
+            }
+
+            return true;
+        }
+      
+
+
+       
+
+        #endregion
+    }
+
+   
+}
---
+++ Class1/Program.cs
@@ -0,0 +1,62 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Human
+            
+
+            //Human human1 = new Human("Ramin", "Aliyev", 24, "Baku");
+            //Human human2 = new Human("Huseyin", "Huseyinov", 27, "Gence");
+            //Human human3 = new Human("Elvin", "Aliyev", 22, "Baku");
+            //Human human4 = new Human("Cavid", "Aliyev", 30, "istanbul");
+
+            //human1.IsStudent();
+
+            Human human = new Human();
+            //human.CheckCity(human1.City, human2.City);
+
+            //Human human5=new Human("Mehemmed","Memmedli",38,"oguz");
+            //Human human6=new Human("Alim", "Qasimov", 34, "siyezen");
+            //Human human7=new Human("Mehman", "Qasimli", 23, "quba");
+            //Human human8=new Human("Ali", "Aliyev", 21, "baki");
+            //Human human9=new Human("Huseyin", "Cavadov", 3, "sumqayit");
+
+
+            //Human[] arr = { human8, human6,human7,human9,human5 };
+
+            Human[] insanlar = new Human[]
+            {
+             new Human("Mehemmed","Memmedli",38,"mingecevir"),
+             new Human("Alim", "Qasimov", 34, "yevlax"),
+             new Human("Mehman", "Qasimli", 23, "baki"),
+             new Human("Ali", "Aliyev", 21, "yevlax"),
+             new Human("Huseyin", "Cavadov", 3, "mingecevir"),
+            };
+
+
+
+
+
+            foreach (var insan in insanlar)
+            {
+                var (seher, count, telebeadlari) = insan.GetCount(insanlar);
+                if (count > 1)
+                {
+                    Console.WriteLine($"{seher} seherinde {count} telebe var: {telebeadlari}");
+                }
+            }
+            #endregion
+
+
+        }
+    }
+}
